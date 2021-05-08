3ebc220eb7209c4e0abc897b93658260ffb81cb3
XxhaimanxX <33806355+XxhaimanxX@users.noreply.github.com>
2021-05-08 18:32:09 +0300

    Head Content component added grid css to mentor page

--- frontend/pages/mentors/index.js
+++ frontend/pages/mentors/index.js
@@ -1,9 +1,26 @@
-import Layout from "../../components/Layout";
+import Layout from '@/components/Layout'
+import HeadContent from '@/components/HeadContent'
+import style from '@/styles/Mentors.module.css'
 
 export default function MentorPage() {
-    return (
-        <Layout>
-            <h1>Mentors Page</h1>
-        </Layout>
-    )
+  return (
+    <>
+      {/* Head content + Categories Carusel*/}
+      <Layout>
+        <HeadContent
+          title="Mentors"
+          content="Proven experts that connect with students and focus on thier
+            individual needs"
+        />
+        <div className={style.wrapper}>
+          <div className={style.box1}>Box 1</div>
+          <div className={style.box2}>Box 2</div>
+          <div className={style.box3}>Box 3</div>
+          <div className={style.box4}>Box 4</div>
+          <div className={style.box5}>Box 5</div>
+          <div className={style.box6}>Box 6</div>
+        </div>
+      </Layout>
+    </>
+  )
 }
