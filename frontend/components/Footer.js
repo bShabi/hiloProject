47b71492e17cb1a09dbfdacc3ed1cf2cc98cae92
DESKTOP-7CIRKLS\almog <almog0523161505@gmail.com>
2021-05-23 00:32:56 +0300

    edit the footer

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -2,16 +2,24 @@ import style from '@/styles/Footer.module.css'
 import Layout from './Layout'
 
 
+// Good Luck 3 //
 
-// Good Luck //
-// Good Luck2 //
 export default function Footer() {
     return (
+        
         <footer className={style.Footer}>
+
+      
            <div className={style.container}>
                <div className={style.row}>
 
-                <div className={style.footerCol}>
+               <div className={style.footerCol}>
+                
+                  <img src="public/images/demo-pic.jpg" alt="logo" />
+                 
+
+              </div>
+               <div className={style.footerCol}>
                     <ul>
                     <h3>Explore Our Content</h3>
                     <li><a href="#">Tips & Hacks</a></li>
@@ -19,6 +27,8 @@ export default function Footer() {
                     <li><a href="#">Courses</a></li>
                     </ul>
                 </div>
+
+             
 
                 
                 <div className={style.footerCol}>
@@ -31,21 +41,33 @@ export default function Footer() {
                     </ul>
                 </div>
 
-                <div className={style.footerCol}>
+                <div className={style.footerCol2}>
                 <h3>Follow Us on Social Media</h3>
                     <div className={style.socialLinks}>
-                    <a href="#"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#"><i class="fab fa-instagram"></i></a>
-  	 				<a href="#"><i class="fab fa-twitter"></i></a>
-  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="#"><i class="fab fa-facebook-f fa-lg"></i></a>
+                    <a href="#"><i class="fab fa-instagram fa-lg"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter fa-lg"></i></a>
+  	 				<a href="#"><i class="fab fa-linkedin-in fa-lg"></i></a>
                     </div>
-                    <h2>Have any Questions?</h2>   
-                    <h4>Please feel free to contact us at </h4> 
-                   
+
+                    <h2>Have any Questions? 
+                    <h4>Please feel free to contact us at&nbsp;
+                    <a href="mailto:info@hilostudent.com"><i>info@hilostudent.com</i></a>
+                    </h4> </h2> 
                 </div>
+
+               
                
                </div>
            </div>
+
+           
+
+                   
+               
+
+
+
         </footer>
         
         
